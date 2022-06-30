17fb056ca8d9938d89ef79de22600c14b0b7085c
Sean Dylan Patterson <74496368+SeanDylan1982@users.noreply.github.com>
2022-06-30 12:40:31 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+
+	<style type="text/css">
+		body{
+			font-family: tahoma;
+			font-size: 13px;
+		}
+	</style>
+
+<h1>My Website</h1>
+<br>
+<br>
+<br>
+
+	<div>
+		Playlist:<br>
+		<div class="PLAYER-PLAYLIST">
+ 			<div path="uploads/04.Lullaby Ft Red Bandanna.mp3">04.Lullaby Ft Red Bandanna.mp3</div>
+			<div path="uploads/03.My love Ft Melarie.mp3">03.My love Ft Melarie.mp3</div>
+			<div path="uploads/02.Last forever Ft Xukari.mp3">02.Last forever Ft Xukari.mp3</div>
+			<div path="uploads/08.In the rain.mp3">08.In the rain.mp3</div>
+		</div>
+	</div>
+
+ 	<?php include "player/player.php" ?>
+ 
